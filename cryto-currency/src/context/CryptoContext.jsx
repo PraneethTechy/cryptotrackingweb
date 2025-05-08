0a024c5510f9385301015612218937446815ce84
PraneethTechy <183295225+PraneethTechy@users.noreply.github.com>
2025-05-08 14:26:24 +0000

    Initial commit
    Created from https://vercel.com/new

---
+++ cryto-currency/src/context/CryptoContext.jsx
@@ -0,0 +1,74 @@
+import { createContext, useState, useEffect } from 'react';
+
+
+
+export const CryptoContext = createContext();
+
+const CryptoContextProvider = (props) => {
+    
+    const [cryptoList,setCryptoList] = useState([]);
+    const [filteredCryptos,setFilteredCryptos] =useState([]);
+    const [searchTerm , setSearchTerm] = useState("");
+    const [currentCurrency , setCurrentCurrency] = useState({
+        name: "usd",
+        Symbol: "$",
+
+    })
+
+    // API CG-dk7hqA7yGLMtZcWMRwx7bu23
+
+    const fetchCryptoData = async () => {
+        const options = {
+            method: 'GET',
+            headers: {accept: 'application/json', 'x-cg-demo-api-key': 'CG-dk7hqA7yGLMtZcWMRwx7bu23'}
+    };
+
+    try{
+        const res = await fetch(
+            `https://api.coingecko.com/api/v3/coins/markets?vs_currency=${currentCurrency.name}`,
+            options
+        );
+        const data = await res.json();
+        setCryptoList(data);
+    } catch(err){
+        console.error("Failed to fetch crypto data:",err);
+    }
+ 
+}
+    // RE FETCH WHEN CURRENCY CHANGES
+
+    useEffect(() => {
+        fetchCryptoData();
+    }, [currentCurrency]);
+
+    // RE FILTER CRYPTOS BY SEARCH TERM CHANGES
+
+    useEffect(() => {
+        if(searchTerm.trim() === ""){
+            setFilteredCryptos(cryptoList);
+        }
+        else{
+            setFilteredCryptos(
+                cryptoList.filter((c) => c.name.toLowerCase().includes(searchTerm.toLowerCase()))
+            )
+        }
+    },[cryptoList, searchTerm]);
+
+    const contextValue = {
+        cryptoList,
+        filteredCryptos,
+        currentCurrency,
+        setCurrentCurrency, 
+         searchTerm,
+        setSearchTerm,
+    }
+
+   
+    return (
+        <CryptoContext.Provider value={contextValue}>
+            {props.children}
+        </CryptoContext.Provider>
+    )
+}
+
+export default CryptoContextProvider ;
